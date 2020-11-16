1480d72a93892555dc9eadf25a7e2c0b934b59b5
JosueChorro <josue14149@gmail.com>
2020-11-16 10:23:14 -0600

    Inicial

---
+++ index.php
@@ -0,0 +1,196 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="icon/fonts.css">
+    <title>Bebidas Refrescantes</title>
+  </head>
+  <body>
+    <!-- Card principal -->
+    <div class="card bg-dark text-white">
+        <!-- Imagen -->
+        <img src="img/fondo.jpeg" class="card-img img-fluid" alt="...">
+        <div class="card-img-overlay">
+            <!-- Menu navbar -->
+            <nav class="navbar navbar-expand-lg">
+                <a class="navbar-brand" href="#">
+                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-grid-fill ico" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5v-3zm8 0A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5v-3zm-8 8A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5v-3zm8 0A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5v-3z"/>
+                </svg>
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-2"></div>
+                        <div class="col-2"></div>
+                        <div class="col-2">
+                            <div class="collapse navbar-collapse " id="navbarNavAltMarkup">
+                                <div class="navbar-nav ">
+                                    <a class="nav-link active" style="color:white; font-size: 18pt;" href="#">Home <span class="sr-only">(current)</span></a>
+                                    <a class="nav-link " style="color:white; font-size: 18pt;" href="#">Smoothies</a>
+                                    <a class="nav-link" style="color:white; font-size: 18pt;" href="#">About</a>
+                                    <a class="nav-link" style="color:white; font-size: 18pt;" href="#">Contact</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+            <div class="row">
+                <!-- Slide izquierda -->
+                <section class="col-3 bloque1">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-2"></div>
+                            <div class="col-9">
+                                <h1 class="morado titulo">Enjoy!</h1>
+                                <br>
+                                <p class="text-justify">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus eaque debitis nesciunt ut? 
+                                    Est magni alias provident reprehenderit architecto, sint itaque eligendi. Magni repellat, veniam non illum adipisci perspiciatis ipsam.
+                                </p>
+                                <br>
+                                <form class="form-inline">
+                                    <input class="form-control mr-sm-2 bor morado" type="search" placeholder="Search" aria-label="Search">
+                                    <button class="btn my-2 my-sm-0 btn-circle btn-xl boton" type="submit">
+                                        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-search ico" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                            <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
+                                            <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
+                                        </svg>
+                                    </button>
+                                </form>
+                            </div>
+                            <div class="col-1"></div>
+                        </div>
+                    </div>
+                </section>
+                <!-- columna de cards -->
+                <section class="col-8 bloque2">
+                    <div class="card-deck">
+                        <!-- Menus verticales -->
+                        <slide class="social"> <!-- Menu de redes -->
+                            <ul>
+                                <li><a href="#" class="icon-facebook"></a></li>
+                                <li><a href="#" class="icon-twitter"></a></li>
+                                <li><a href="#" class="icon-instagram"></a></li>
+                            </ul>
+                        </slide>
+                        <!-- Puntos de seccion -->
+                        <slide class="puntos">
+                            <ul>
+                                <li><a href="#">
+                                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-circle-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                        <circle cx="4" cy="4" r="4"/>
+                                    </svg>
+                                </a></li>
+                                <li><a href="#">
+                                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-circle-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                        <circle cx="4" cy="4" r="4"/>
+                                    </svg>
+                                </a></li>
+                                <li><a href="#">
+                                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-circle-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                        <circle cx="4" cy="4" r="4"/>
+                                    </svg>
+                                </a></li>
+                                        <li><a href="#">
+                                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-circle-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                                <circle cx="4" cy="4" r="4"/>
+                                            </svg>
+                                        </a></li>
+                                    </ul>
+                            </slide>
+                        <!-- Card 1 -->
+                        <div class="card tam card1 bor">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-4">
+                                        <h5 class="card-title aum1">Blueberry Special</h5>
+                                    </div>
+                                    <div class="col-5"></div>
+                                    <div class="col-3">
+                                        <p class="btn-circle circulo1 ">20%</p>
+                                    </div>
+                                </div>
+                                <br>
+                                <div class="row">
+                                    <div class="col-5">
+                                        <p>Calories: 20 Kcal    Size: 10 oz</p>
+                                    </div>
+                                    <div class="col-4"></div>
+                                    <div class="col-3">
+                                        <p class="aum"><b>$12</b></p>
+                                    </div>
+                                </div>                                
+                            </div>
+                        </div>
+                        <!-- Card 2 -->
+                        <div class="card tam card2 bor">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-4">
+                                        <h5 class="card-title aum1">Blueberry Special</h5>
+                                    </div>
+                                    <div class="col-5"></div>
+                                    <div class="col-3">
+                                        <p class="btn-circle circulo2">20%</p>
+                                    </div>
+                                </div>
+                                <br>
+                                <div class="row">
+                                    <div class="col-5">
+                                        <p>Calories: 20 Kcal    Size: 10 oz</p>
+                                    </div>
+                                    <div class="col-4"></div>
+                                    <div class="col-3">
+                                        <p class="aum"><b>$12</b></p>
+                                    </div>
+                                </div>                                
+                            </div>
+                        </div>
+                        <!-- Card 3 -->
+                        <div class="card tam card3 bor">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-4">
+                                        <h5 class="card-title aum1">Blueberry Special</h5>
+                                    </div>
+                                    <div class="col-5"></div>
+                                    <div class="col-3">
+                                        <p class="btn-circle circulo3 ">20%</p>
+                                    </div>
+                                </div>
+                                <br>
+                                <div class="row">
+                                    <div class="col-5">
+                                        <p>Calories: 20 Kcal    Size: 10 oz</p>
+                                    </div>
+                                    <div class="col-4"></div>
+                                    <div class="col-3">
+                                        <p class="aum"><b>$12</b></p>
+                                    </div>
+                                </div>                                
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
+    
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+
+  </body>
+</html>
